b04624291d288c82928a180f94dbfe53598ffe64
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2020-06-23 20:11:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+function validateForm() {
+    var x = document.getElementsByName("first_name").value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("last_name").value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("age").value;
+    if (x == "") {
+      alert("age must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("phone").value;
+    if (x == "") {
+      alert("phone must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("School").value;
+    if (x == "") {
+      alert("school must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("tenth").value;
+    if (x == "") {
+      alert("Marks must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("email").value;
+    if (x == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("pu").value;
+    if (x == "") {
+      alert("Pu college must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("pumarks").value;
+    if (x == "") {
+      alert("pu marks  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("ug").value;
+    if (x == "") {
+      alert("ug must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("uggrade").value;
+    if (x == "") {
+      alert("ug marks  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("pg").value;
+    if (x == "") {
+      alert("pg must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("uggrade").value;
+    if (x == "") {
+      alert("pg marks  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("why").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("experience").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("about").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("project").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("goodat").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    var x = document.getElementsByName("long").value;
+    if (x == "") {
+      alert("data  must be filled out");
+      return false;
+    }
+    
+  } 
